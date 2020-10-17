8645d80d3c855a16a28308dda95bf9e335c1eb81
Mike Navrotsky <mikes.navr@gmail.com>
2020-10-17 11:32:10 +0200

    day2

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-let game = document.querySelector('.game');
+const game = document.querySelector('.game');
+const result = document.querySelector('.result');
 
-let field = ['.a1', '.a2', '.a3', '.b1', '.b2', '.b3', '.c1', '.c2', '.c3'];
+let cross = 'X';
+let circle = 'O';
+let count = 0;
 
-let mark = 'X';
+let field = {
+    a1: ['a1', false],
+    a2: ['a2', false],
+    a3: ['a3', false],
+    a4: ['b1', false],
+    a5: ['b2', false],
+    a6: ['b3', false],
+    a7: ['c1', false],
+    a8: ['c2', false],
+    a9: ['c3', false],
+}
 
-field.forEach((element)=>{
-    document.querySelector(`${element}`).addEventListener('click', ()=>{
-        document.querySelector(`${element}`).innerHTML = mark;
-    })    
-});
+for ( let key in field ){
+    document.querySelector(`.${field[key][0]}`).addEventListener('click', ()=> {
+        if ( field[key][1] == false ){
+            if ( count % 2 ){
+                document.querySelector(`.${field[key][0]}`).innerHTML = circle;
+                field[key].push('circle');
+            }else{
+                document.querySelector(`.${field[key][0]}`).innerHTML = cross;
+                field[key].push('cross');
+            }
+            field[key][1] = true;
+            count++;
+        }
+    });
+}
+
+function checkWinner(){
+    let allCells = document.getElementsByClassName('cell');
+    if (allCells[0].innerHTML == cross && allCells[1].innerHTML == cross && allCells[2].innerHTML == cross){
+        alert('win')
+    }
+}
+checkWinner();
