5e464a1f39c4c15eab5f067f29396f26ae8e92df
Mike Navrotsky <mikes.navr@gmail.com>
2020-10-22 07:59:56 +0200

    day3

--- script.js
+++ script.js
@@ -1,42 +1,40 @@
-const game = document.querySelector('.game');
-const result = document.querySelector('.result');
+let turnMessage = document.querySelector('.turn-message'),
+    board = document.querySelector('.board'),
+    cells = document.querySelectorAll('.cell'),
+    result = document.querySelector('.result'),
+    start = document.querySelector('.game-start'),
+    player = 'X'
+    arrX = [],
+    arrO = [];
 
-let cross = 'X';
-let circle = 'O';
-let count = 0;
+start.addEventListener("click", startGame);
 
-let field = {
-    a1: ['a1', false],
-    a2: ['a2', false],
-    a3: ['a3', false],
-    a4: ['b1', false],
-    a5: ['b2', false],
-    a6: ['b3', false],
-    a7: ['c1', false],
-    a8: ['c2', false],
-    a9: ['c3', false],
-}
-
-for ( let key in field ){
-    document.querySelector(`.${field[key][0]}`).addEventListener('click', ()=> {
-        if ( field[key][1] == false ){
-            if ( count % 2 ){
-                document.querySelector(`.${field[key][0]}`).innerHTML = circle;
-                field[key].push('circle');
-            }else{
-                document.querySelector(`.${field[key][0]}`).innerHTML = cross;
-                field[key].push('cross');
-            }
-            field[key][1] = true;
-            count++;
-        }
+function startGame(){
+    cells.forEach((cell) =>{
+        cell.innerHTML = '';
     });
 }
 
-function checkWinner(){
-    let allCells = document.getElementsByClassName('cell');
-    if (allCells[0].innerHTML == cross && allCells[1].innerHTML == cross && allCells[2].innerHTML == cross){
-        alert('win')
-    }
+for ( i = 0; i < cells.length; i++ ){
+    cells[i].addEventListener('click', drawMark, {once: true})
+    if ( cells[i].innerHTML === 'X' ) arrX.push(i);
+    else arrO.push(i);
+};
+
+console.log(arrX);
+console.log(arrO);
+
+function drawMark(e){
+    e.target.innerHTML = player;
+    changePlayer();
+    turnMessage.innerHTML = `It is ${player} turn`;
+
 }
-checkWinner();
+
+function changePlayer(){
+    player === 'X' ? (player = 'O'): (player = 'X');
+}
+
+function checkWinner(){
+
+}
